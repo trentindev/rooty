1b3006539aa710e96d22b44aa81c2ecb2abad9b5
Laurent Dislaire <siladire@free.fr>
2024-07-05 09:41:53 +0200

    create styled component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,25 @@
 //src/components/Header/index.jsx
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const StyledLink = styled(Link)`
+  padding: 15px;
+  color: #8186a0;
+  text-decoration: none;
+  font-size: 18px;
+  ${(props) =>
+    props.$isFullLink &&
+    `color: white; border-radius: 30px; background-color: #5843E4;`}
+`;
 
 function Header() {
   return (
     <nav>
-      <Link to="/">Accueil</Link>
-      <Link to="/survey">Questionnaire</Link>
-      <Link to="/profils">Freelances</Link>
+      <StyledLink to="/">Accueil</StyledLink>
+      <StyledLink to="/survey/1" $isFullLinks>
+        Questionnaire
+      </StyledLink>
+      <StyledLink to="/freelances">Profils</StyledLink>
     </nav>
   );
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,7 @@
-import Freelances from "../Freelances";
-
 function Home() {
   return (
     <>
       <h1>Home 🏠</h1>
-      <Freelances />
     </>
   );
 }
